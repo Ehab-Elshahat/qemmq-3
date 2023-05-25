8e8e0c37a7da4f2ec32377428641739ce33a7cd4
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2023-05-25 14:38:41 +0300

    cons page 1

--- js/index.js
+++ js/index.js
@@ -204,44 +204,53 @@ serTitles.forEach((title, index) => {
 
 // Consulting Page
 let titleContainer = document.querySelector(".cons-content aside");
-let conTitles = document.querySelectorAll(".cons-content aside h3");
+let conTitles = document.querySelectorAll(".cons-content aside h3"); 
 
 let contents = document.querySelectorAll(".content");
 
-conTitles[0].addEventListener("click", () => {
-  window.scrollTo(0, 300);
-});
-conTitles[1].addEventListener("click", () => {
-  window.scrollTo(0, 950);
-});
-conTitles[2].addEventListener("click", () => {
+conTitles[0].addEventListener("click",()=> {
+  window.scrollTo(0,300)
+})
+conTitles[1].addEventListener("click",()=> {
+  window.scrollTo(0,950)
+})
+conTitles[2].addEventListener("click",()=> {
   window.scrollTo(0, 1930);
-});
+})
 
-window.addEventListener("scroll", () => {
-  let scrollValu = window.pageYOffset;
-  if (scrollValu > 220) {
-    titleContainer.classList.add("fixed");
-  } else {
-    titleContainer.classList.remove("fixed");
-  }
-  if (scrollValu >= 2250) {
-    titleContainer.classList.remove("fixed");
-  }
 
-  if (scrollValu >= 300) {
-    conTitles[0].classList.add("active", "b");
-  } else {
-    conTitles[0].classList.remove("active", "b");
-  }
-  if (scrollValu >= 950) {
-    conTitles[1].classList.add("active", "b");
-  } else {
-    conTitles[1].classList.remove("active", "b");
-  }
-  if (scrollValu >= 1930) {
-    conTitles[2].classList.add("active", "b");
-  } else {
-    conTitles[2].classList.remove("active", "b");
-  }
-});
+
+window.addEventListener("scroll",()=> {
+
+ let scrollValu = window.pageYOffset;
+if(scrollValu > 220) {
+  titleContainer.classList.add("fixed");
+} else {
+  titleContainer.classList.remove("fixed");
+}
+ if (scrollValu >= 2250) {
+  titleContainer.classList.remove("fixed");
+  titleContainer.classList.add("end-section");
+ } else {
+  titleContainer.classList.remove("end-section");
+ }
+
+ if(scrollValu >= 300 ) {
+  conTitles[0].classList.add("active", "b")
+ } else {
+  conTitles[0].classList.remove("active", "b");
+ }
+ if (scrollValu >= 950) {
+   conTitles[1].classList.add("active", "b");
+ } else {
+   conTitles[1].classList.remove("active", "b");
+ }
+ if (scrollValu >= 1930) {
+   conTitles[2].classList.add("active", "b");
+ } else {
+   conTitles[2].classList.remove("active", "b");
+ }
+
+
+
+})
